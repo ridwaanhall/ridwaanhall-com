71a7366b678ed3824ad7d187220bab6865ebdade
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-16 16:26:13 +0700

    chore: restructure project to use ridwaanhall_dev settings and remove old files

--- ridwaanhall_dev/wsgi.py
+++ ridwaanhall_dev/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RIDWAANHALL project.
+WSGI config for ridwaanhall_dev project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RIDWAANHALL.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ridwaanhall_dev.settings')
 
 application = get_wsgi_application()

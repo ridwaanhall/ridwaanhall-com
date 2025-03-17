ee60ef3ca5110685fc5b9f1d132d02800cd904ac
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-17 17:03:53 +0700

    feat: add duplicate application instance in WSGI configuration

--- ridwaanhall_dev/wsgi.py
+++ ridwaanhall_dev/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ridwaanhall_dev.settings.dev')
 
 application = get_wsgi_application()
+
+app = get_wsgi_application()

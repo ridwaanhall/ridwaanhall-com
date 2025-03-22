b3cc8d5b43532f3a34bab7632441512d07ec0d9b
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-22 15:31:35 +0700

    feat: add Career view and corresponding URL routing

--- apps/career/views.py
+++ apps/career/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class CareerView(TemplateView):
+    template_name = 'career/career.html'

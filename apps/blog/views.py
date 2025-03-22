ed202be10ef2d1987acde6dac4952e16dd6dcfbf
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-22 15:54:59 +0700

    feat: add blog, projects, and dashboard views with corresponding URL routing and templates

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class BlogView(TemplateView):
+    template_name = 'blog/blog_list.html'
--- ridwaanhall_dev/urls.py
+++ ridwaanhall_dev/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('career/', include('apps.career.urls')),
+    path('dashboard/', include('apps.dashboard.urls')),
+    path('projects/', include('apps.projects.urls')),
+    path('blog/', include('apps.blog.urls')),
 ]

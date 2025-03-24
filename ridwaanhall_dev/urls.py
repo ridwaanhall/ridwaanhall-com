925197d7db3c1b6811a6f9ca07f05bdf38a7015c
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-24 22:22:03 +0700

    feat: add CatchAllView to handle undefined routes with a 404 error page

--- ridwaanhall_dev/urls.py
+++ ridwaanhall_dev/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,6 @@ urlpatterns = [
     path('dashboard/', include('apps.dashboard.urls')),
     path('projects/', include('apps.projects.urls')),
     path('blog/', include('apps.blog.urls')),
+    
+    path('<path:url>', views.CatchAllView.as_view(), name='catch_all'),
 ]

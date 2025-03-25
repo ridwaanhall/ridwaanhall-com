d305aadd8d062d1ac783e24052be6e6c2f185476
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-25 15:34:14 +0700

    refactor: simplify database settings for development environment

--- ridwaanhall_dev/settings.py
+++ ridwaanhall_dev/settings.py
@@ -96,27 +96,12 @@ WSGI_APPLICATION = 'ridwaanhall_dev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DATABASE'),
-            'USER': config('USER'),
-            'PASSWORD': config('PASSWORD'),
-            'HOST': config('HOST'),
-            'PORT': '6543',
-            'OPTIONS': {
-                'sslmode': 'require',
-            },
-        }
-    }
+}
 
 
 # Password validation

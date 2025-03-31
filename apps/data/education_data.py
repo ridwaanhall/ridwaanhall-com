81e42ee86f6852b764c323574d33d4ca6a2c3b9d
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-31 20:25:13 +0700

    feat: update education data with additional degrees and achievements, and enhance logo and website display in template

--- apps/data/education_data.py
+++ apps/data/education_data.py
@@ -1,9 +1,11 @@
 class EducationData:
     education = [
         {
-            "degree": "B.Tech in Artificial Intelligence",
+            "degree": "S.Kom in Intelligence Systems (AI)",
             "years": "2021 - 2025",
             "institution": "Universitas Teknologi Yogyakarta",
+            "website": "https://uty.ac.id/",
+            "logo": "https://ridwaanhall.me/static/img/edu/uty.webp",
             "is_last": True,
             "achievements": [
                 "Studied Algorithm Programming and Object-Oriented Programming",
@@ -17,21 +19,49 @@ class EducationData:
             "degree": "Associate of Science in Natural Sciences",
             "years": "2018 - 2021",
             "institution": "MAS Al Mukmin Ngruki",
+            "website": "https://almukminngruki.or.id/",
+            "logo": "https://ridwaanhall.me/static/img/edu/al_mukmin_ngruki.webp",
             "is_last": False,
             "achievements": [
+                "Practiced reviewing and memorizing the Qur'an before and after salah (prayer).",
+                "Engaged in environmental and outdoor activities as part of the society.",
+                "Memorized nearly the entire Qur'an with remarkable dedication and focus.",
                 "Participated in Santri Pecinta Alam (SAPALA KAMUFISA).",
-                "Engaged in environmental and outdoor activities as part of the society."
             ]
         },
         {
-            "degree": "Junior High School Diploma",
+            "degree": "Junior High School",
             "years": "2015 - 2018",
             "institution": "MTsS Islam Ngruki",
+            "website": "https://almukminngruki.or.id/",
+            "logo": "https://ridwaanhall.me/static/img/edu/al_mukmin_ngruki.webp",
             "is_last": False,
             "achievements": [
-                "Memorized portions of the Quran.",
+                "Practiced reviewing and memorizing the Qur'an before and after salah (prayer).",
                 "Practiced archery and taught Islamic religious knowledge.",
                 "Learned foundational web development skills including HTML, CSS, and PHP."
+            ]
+        },
+        {
+            "degree": "Elementary School",
+            "years": "2009 - 2015",
+            "institution": "SD IT Al Mannan",
+            "is_last": False,
+            "achievements": [
+                "Practiced daily prayers, recitations for salah, and post-salah supplications.",
+                "Memorized Juz 30 of the Qur'an and parts of Juz 29.",
+                "Learned stories and historical accounts of prophets and Islam."
+            ]
+        },
+        {
+            "degree": "Kindergarten",
+            "years": "2007 - 2009",
+            "institution": "TK IT Al Mannan",
+            "is_last": False,
+            "achievements": [
+                "Practiced and performed daily Islamic prayers.",
+                "Memorized short Qur'anic surahs.",
+                "Learned and expanded everyday vocabulary in both Arabic and English."
             ]
         }
     ]

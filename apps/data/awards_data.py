d600fee20cbf36f87462f99f1ed1a3d531446019
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-05-03 17:22:31 +0700

    feat(data): add awards data with detailed descriptions and credential URLs

---
+++ apps/data/awards_data.py
@@ -0,0 +1,66 @@
+from django.conf import settings
+
+
+class AwardsData:
+    awards = [
+        {
+            "id": 1,
+            "title": "شهادة شكر وتقدير على اجتهادة في حفظ القرآن الكريم",
+            "credential_url": "https://drive.google.com/file/d/1-XJlFZzkq00QM9n2m7lh4m641nlT8jod/view",
+            "description": "حفظ 30 أجزاء من القرآن الكريم",
+            "issued": "Feb 2020",
+            "institution": "معهد المؤمن للتربية الإسلامية",
+            "website": "https://almukminngruki.or.id/",
+            "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
+        },
+        {
+            "id": 2,
+            "title": "Certificate of Scholarship for Memorizing 10 Juz of the Quran",
+            "credential_url": "https://drive.google.com/file/d/1-L8O8h1ErOH2HqKFnMDDVazr-tEpJGEd/view",
+            "description": "I was awarded the Tahfidz Scholarship Certificate for memorizing 10 Juz of the Quran after successfully passing the memorization test. This scholarship is a recognition of my dedication and effort in studying the Quran.",
+            "issued": "Mar 2020",
+            "institution": "معهد المؤمن للتربية الإسلامية",
+            "website": "https://almukminngruki.or.id/",
+            "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
+        },
+        {
+            "id": 3,
+            "title": "Certificate of Appreciation for Leadership in Imarotu Syu'unith Tholabah (IST)",
+            "credential_url": "https://drive.google.com/file/d/1-4ciNM2bKyR9fDWoS8t7gU30vkrqvtVc/view",
+            "description": "Awarded for leadership and organizational contributions in IST, a student council-like organization at Al-Mukmin Islamic Boarding School.",
+            "issued": "Oct 2020",
+            "institution": "معهد المؤمن للتربية الإسلامية",
+            "website": "https://almukminngruki.or.id/",
+            "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
+        },
+        {
+            "id": 4,
+            "title": "Community Top Voice in Programming",
+            "credential_url": "https://www.linkedin.com/in/ridwaanhall/details/honors/",
+            "description": "Recognized as a top 5% contributor in Programming on LinkedIn, showcasing expertise and insightful contributions in the field.",
+            "issued": "Jul 2024",
+            "institution": "LinkedIn",
+            "website": "https://www.linkedin.com/",
+            "logo": f"{settings.BASE_URL}/static/img/logo/linkedin.webp",
+        },
+        {
+            "id": 5,
+            "title": "Community Top Voice in Machine Learning",
+            "credential_url": "https://www.linkedin.com/in/ridwaanhall/details/honors/",
+            "description": "Recognized as a top 5% contributor in Machine Learning on LinkedIn, showcasing expertise and insightful contributions in the field.",
+            "issued": "Aug 2024",
+            "institution": "LinkedIn",
+            "website": "https://www.linkedin.com/",
+            "logo": f"{settings.BASE_URL}/static/img/logo/linkedin.webp",
+        },
+        {
+            "id": 6,
+            "title": "Community Top Voice in Informatics",
+            "credential_url": "https://www.linkedin.com/in/ridwaanhall/details/honors/",
+            "description": "Recognized as a top 5% contributor in Informatics on LinkedIn, showcasing expertise and insightful contributions in the field.",
+            "issued": "Oct 2024",
+            "institution": "LinkedIn",
+            "website": "https://www.linkedin.com/",
+            "logo": f"{settings.BASE_URL}/static/img/logo/linkedin.webp",
+        },
+    ]

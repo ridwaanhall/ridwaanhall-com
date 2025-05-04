6a0c2e498e931fdd35e1e4cad43f4cb0c6d25478
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-05-05 01:17:10 +0700

    feat(education): enhance education section with location details and map links

--- apps/data/education_data.py
+++ apps/data/education_data.py
@@ -9,6 +9,14 @@ class EducationData:
             "website": "https://uty.ac.id/",
             "logo": f"{settings.BASE_URL}/static/img/logo/uty.webp",
             "is_last": True,
+            "location": {
+                "regency": "Sleman",
+                "province": "Special Region of Yogyakarta",
+                "prov": "Yogyakarta",
+                "country": "Indonesia",
+                "flag": "🇮🇩",
+                "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.1386717050764!2d110.35389748855174!3d-7.747286929566545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a58f2d747cc8d%3A0xba7c703a016a750e!2sUniversity%20of%20Technology%20Yogyakarta!5e1!3m2!1sen!2sid!4v1746378720413!5m2!1sen!2sid"
+            },
             "achievements": [
                 "Got down with algorithms and object-oriented coding—nailed the basics!",
                 "Built slick web in hands-on courses.",
@@ -18,12 +26,20 @@ class EducationData:
             ]
         },
         {
-            "degree": "Associate of Science in Natural Sciences",
+            "degree": "Senior High School (Natural Science)",
             "years": "2018 - 2021",
             "institution": "MAS Al Mukmin Ngruki",
             "website": "https://almukminngruki.or.id/",
             "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
             "is_last": False,
+            "location": {
+                "regency": "Surakarta",
+                "province": "Central Java",
+                "prov": "Central Java",
+                "country": "Indonesia",
+                "flag": "🇮🇩",
+                "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d839.8902086006083!2d110.81010211911801!3d-7.583671478356138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a161053e2e49b%3A0xa3c304c347094dd4!2sPondok%20Pesantren%20Al%20Mukmin!5e1!3m2!1sen!2sid!4v1746378601521!5m2!1sen!2sid"
+            },
             "achievements": [
                 "Memorized Qur’an like a champ, reciting before and after prayers.",
                 "Got my hands dirty with outdoor adventures and community vibes.",
@@ -38,6 +54,14 @@ class EducationData:
             "website": "https://almukminngruki.or.id/",
             "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
             "is_last": False,
+            "location": {
+                "regency": "Surakarta",
+                "province": "Central Java",
+                "prov": "Central Java",
+                "country": "Indonesia",
+                "flag": "🇮🇩",
+                "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d839.8902086006083!2d110.81010211911801!3d-7.583671478356138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a161053e2e49b%3A0xa3c304c347094dd4!2sPondok%20Pesantren%20Al%20Mukmin!5e1!3m2!1sen!2sid!4v1746378601521!5m2!1sen!2sid"
+            },
             "achievements": [
                 "Kept up with Qur’an recitation around prayer times—soul fuel.",
                 "Tried my hand at archery and shared Islamic knowledge with peers.",
@@ -49,6 +73,14 @@ class EducationData:
             "years": "2009 - 2015",
             "institution": "SD IT Al Mannan",
             "is_last": False,
+            "location": {
+                "regency": "Boyolali",
+                "province": "Central Java",
+                "prov": "Central Java",
+                "country": "Indonesia",
+                "flag": "🇮🇩",
+                "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d839.8741719724507!2d110.63054034044244!3d-7.540896804194124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a6bf96222c533%3A0x1eac3559e081d4e2!2sSDIT%20Al%20Mannan%20Mojosongo!5e1!3m2!1sen!2sid!4v1746378437547!5m2!1sen!2sid"
+            },
             "achievements": [
                 "Got into daily prayers and post-salah du’as—built that routine early.",
                 "Memorized Juz 30 and chunks of Juz 29—Qur’an game strong!",
@@ -60,6 +92,14 @@ class EducationData:
             "years": "2007 - 2009",
             "institution": "TK IT Al Mannan",
             "is_last": False,
+            "location": {
+                "regency": "Boyolali",
+                "province": "Central Java",
+                "prov": "Central Java",
+                "country": "Indonesia",
+                "flag": "🇮🇩",
+                "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1009.2575066395403!2d110.63296114927637!3d-7.544367428472007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a6b777ddce74d%3A0x5963db2cb6db1f07!2sTKIT%20AL-MANNAN!5e1!3m2!1sen!2sid!4v1746378514204!5m2!1sen!2sid"
+            },
             "achievements": [
                 "Started praying daily and nailing those Islamic vibes.",
                 "Memorized short Qur’anic surahs—tiny me was killing it!",

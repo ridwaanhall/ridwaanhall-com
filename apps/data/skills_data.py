443d38cdd67780b04b622983e8edf77a3a9fd890
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-05-13 00:13:58 +0700

    style(skills): format skills data for improved readability and maintainability

--- apps/data/skills_data.py
+++ apps/data/skills_data.py
@@ -1,57 +1,215 @@
 class SkillsData:
     skills = [
-        {"title": "Python", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"},
-        {"title": "PHP", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"},
-        {"title": "JavaScript", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"},
-        {"title": "Django", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg"},
-        {"title": "Flask", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg"},
-        {"title": "React", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"},
-        {"title": "Next.js", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"},
-        {"title": "HTML5", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},
-        {"title": "CSS3", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"},
-        {"title": "Bootstrap", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"},
-        {"title": "Tailwind CSS", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg"},
-        {"title": "Bulma", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bulma/bulma-plain.svg"},
-        {"title": "Node.js", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"},
-        {"title": "WordPress", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg"},
-        {"title": "Material-UI", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"},
-        {"title": "TypeScript", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"},
-        {"title": "Laravel", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-original.svg"},
-        {"title": "TensorFlow", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg"},
-        {"title": "PyTorch", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg"},
-        {"title": "Keras", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/keras/keras-original.svg"},
-        {"title": "Scikit-learn", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/scikitlearn/scikitlearn-original.svg"},
-        {"title": "MySQL", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"},
-        {"title": "SQLite", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg"},
-        {"title": "MongoDB", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"},
-        {"title": "PostgreSQL", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"},
-        {"title": "Git", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"},
-        {"title": "GitHub", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"},
-        {"title": "Django REST Framework", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/djangorest/djangorest-original.svg"},
-        {"title": "Postman", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg"},
-        {"title": "GraphQL", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg"},
-        {"title": "Vercel", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg"},
-        {"title": "Netlify", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/netlify/netlify-original.svg"},
-        {"title": "Cloudflare Pages", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cloudflare/cloudflare-original.svg"},
-        # {"title": "Docker", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"},
-        {"title": "Cloudflare Workers", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cloudflareworkers/cloudflareworkers-original.svg"},
-        {"title": "Nginx", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg"},
-        {"title": "Visual Studio Code", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"},
-        {"title": "JetBrains", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg"},
-        {"title": "Figma", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"},
-        {"title": "Canva", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg"},
-        {"title": "Jupyter Notebook", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original.svg"},
-        {"title": "Anaconda", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/anaconda/anaconda-original.svg"},
-        {"title": "OpenCV", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opencv/opencv-original.svg"},
-        {"title": "Pandas", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg"},
-        {"title": "NumPy", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg"},
-        {"title": "Matplotlib", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matplotlib/matplotlib-original.svg"},
-        {"title": "Plotly", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/plotly/plotly-original.svg"},
-        {"title": "Streamlit", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/streamlit/streamlit-original.svg"},
-        {"title": "FastAPI", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg"},
-        {"title": "Selenium", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"},
-        {"title": "PyPi", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pypi/pypi-original.svg"},
-        {"title": "WooCommerce", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/woocommerce/woocommerce-original.svg"},
-        {"title": "WordPress", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg"},
-        {"title": "SQLAlchemy", "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original.svg"},
+        {
+            "title": "Python",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+        },
+        {
+            "title": "PHP",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+        },
+        {
+            "title": "JavaScript",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+        },
+        {
+            "title": "Django",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg"
+        },
+        {
+            "title": "Flask",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg"
+        },
+        {
+            "title": "React",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+        },
+        {
+            "title": "Next.js",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+        },
+        {
+            "title": "HTML5",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+        },
+        {
+            "title": "CSS3",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+        },
+        {
+            "title": "Bootstrap",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+        },
+        {
+            "title": "Tailwind CSS",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg"
+        },
+        {
+            "title": "Bulma",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bulma/bulma-plain.svg"
+        },
+        {
+            "title": "Node.js",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+        },
+        {
+            "title": "Material-UI",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
+        },
+        {
+            "title": "TypeScript",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+        },
+        {
+            "title": "Laravel",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-original.svg"
+        },
+        {
+            "title": "TensorFlow",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg"
+        },
+        {
+            "title": "PyTorch",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg"
+        },
+        {
+            "title": "Keras",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/keras/keras-original.svg"
+        },
+        {
+            "title": "Scikit-learn",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/scikitlearn/scikitlearn-original.svg"
+        },
+        {
+            "title": "MySQL",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+        },
+        {
+            "title": "SQLite",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg"
+        },
+        {
+            "title": "MongoDB",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+        },
+        {
+            "title": "PostgreSQL",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
+        },
+        {
+            "title": "Git",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+        },
+        {
+            "title": "GitHub",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+        },
+        {
+            "title": "Django REST Framework",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/djangorest/djangorest-original.svg"
+        },
+        {
+            "title": "Postman",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg"
+        },
+        {
+            "title": "GraphQL",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg"
+        },
+        {
+            "title": "Vercel",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg"
+        },
+        {
+            "title": "Netlify",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/netlify/netlify-original.svg"
+        },
+        {
+            "title": "Cloudflare Pages",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cloudflare/cloudflare-original.svg"
+        },
+        # {
+        #     "title": "Docker",
+        #     "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+        # },
+        {
+            "title": "Cloudflare Workers",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cloudflareworkers/cloudflareworkers-original.svg"
+        },
+        {
+            "title": "Nginx",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg"
+        },
+        {
+            "title": "Visual Studio Code",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
+        },
+        {
+            "title": "JetBrains",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg"
+        },
+        {
+            "title": "Figma",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
+        },
+        {
+            "title": "Canva",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg"
+        },
+        {
+            "title": "Jupyter Notebook",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original.svg"
+        },
+        {
+            "title": "Anaconda",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/anaconda/anaconda-original.svg"
+        },
+        {
+            "title": "OpenCV",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opencv/opencv-original.svg"
+        },
+        {
+            "title": "Pandas",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg"
+        },
+        {
+            "title": "NumPy",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg"
+        },
+        {
+            "title": "Matplotlib",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matplotlib/matplotlib-original.svg"
+        },
+        {
+            "title": "Plotly",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/plotly/plotly-original.svg"
+        },
+        {
+            "title": "Streamlit",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/streamlit/streamlit-original.svg"
+        },
+        {
+            "title": "FastAPI",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg"
+        },
+        {
+            "title": "Selenium",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"
+        },
+        {
+            "title": "PyPi",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pypi/pypi-original.svg"
+        },
+        {
+            "title": "WooCommerce",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/woocommerce/woocommerce-original.svg"
+        },
+        {
+            "title": "WordPress",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg"
+        },
+        {
+            "title": "SQLAlchemy",
+            "icon_svg": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original.svg"
+        }
     ]

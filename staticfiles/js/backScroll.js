de7153d0dabb0a61b2b1796924a71c56b15fc6e0
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-06-07 20:10:55 +0700

    feat(scroll-buttons): implement scroll to top and back to blog buttons with visibility toggle

--- staticfiles/js/backScroll.js
+++ staticfiles/js/backScroll.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    const backToBtn = document.getElementById('backToBtn');
     
     window.addEventListener('scroll', function() {
         if (window.scrollY > 300) {
             scrollToTopBtn.classList.remove('opacity-0', 'translate-y-10');
             scrollToTopBtn.classList.add('opacity-100', 'translate-y-0');
+            backToBtn.classList.remove('opacity-0', 'translate-y-10');
+            backToBtn.classList.add('opacity-100', 'translate-y-0');
         } else {
             scrollToTopBtn.classList.remove('opacity-100', 'translate-y-0');
             scrollToTopBtn.classList.add('opacity-0', 'translate-y-10');
+            backToBtn.classList.remove('opacity-100', 'translate-y-0');
+            backToBtn.classList.add('opacity-0', 'translate-y-10');
         }
     });
     

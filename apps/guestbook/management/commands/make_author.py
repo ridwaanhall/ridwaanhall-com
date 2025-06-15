a95a3e8daf5109240b9d94b33347fe44ab8cb08d
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-06-15 22:43:49 +0700

    fix(import): correct import path for UserProfile in make_author command (#55)

--- apps/guestbook/management/commands/make_author.py
+++ apps/guestbook/management/commands/make_author.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from guestbook.models import UserProfile
+from apps.guestbook.models import UserProfile
 
 
 class Command(BaseCommand):

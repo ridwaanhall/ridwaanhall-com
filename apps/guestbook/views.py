ec04fb3aecbdb3ff79f523b1855ba616c117cf04
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-06-20 04:31:19 +0700

    fix(blog): correct static template tag syntax and clean up whitespace

--- apps/guestbook/views.py
+++ apps/guestbook/views.py
@@ -176,7 +176,8 @@ class SendMessageView(LoginRequiredMixin, UserProfileMixin, View):
             reply_data = {
                 'id': reply_to_message.pk,
                 'user': reply_profile_data['full_name'],
-                'message': reply_to_message.message[:50] + ('...' if len(reply_to_message.message) > 50 else ''),                'profile_image': reply_profile_data['profile_image'],
+                'message': reply_to_message.message[:50] + ('...' if len(reply_to_message.message) > 50 else ''),
+                'profile_image': reply_profile_data['profile_image'],
                 'is_author': reply_profile_data['is_author'],
                 'is_co_author': reply_profile_data['is_co_author'],
                 'co_author_order': reply_profile_data['co_author_order']

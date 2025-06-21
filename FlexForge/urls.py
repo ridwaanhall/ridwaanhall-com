7f831b4a98a9406427e0447e578d7340eadacdec
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-06-21 16:08:01 +0700

    feat: conditionally include guestbook URLs based on settings

--- FlexForge/urls.py
+++ FlexForge/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -8,11 +9,18 @@ urlpatterns = [
     path('projects/', include('apps.projects.urls')),
     path('blog/', include('apps.blog.urls')),
     path('about/', include('apps.about.urls')),
-    path('guestbook/', include('apps.guestbook.urls')),
     
     # Add favicon path before the catch-all
     path('favicon.ico', views.favicon_view, name='favicon'),
 ]
+
+# Conditionally include guestbook URLs (which includes allauth URLs)
+if getattr(settings, 'GUESTBOOK_PAGE', True):
+    urlpatterns.insert(-1, path('guestbook/', include('apps.guestbook.urls')))
+else:
+    # If guestbook is disabled, we might still want basic allauth URLs for admin access
+    # But since allauth apps won't be installed, we skip this entirely
+    pass
 
 # Custom error handlers
 handler404 = 'FlexForge.views.custom_404_view'

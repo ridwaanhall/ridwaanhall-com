cd9058a471d3b0aede8d82182cc6a3ce75a91f72
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-06-21 16:09:21 +0700

    feat: add context processor for feature flags in templates

---
+++ FlexForge/context_processors.py
@@ -0,0 +1,20 @@
+"""
+Context processors for FlexForge project.
+Makes certain settings available in all templates.
+
+Author: Ridwan Halim (ridwaanhall.com)
+License: Apache License 2.0
+"""
+
+from django.conf import settings
+
+
+def feature_flags(request):
+    """
+    Context processor to make feature flags available in templates.
+    """
+    guestbook_enabled = getattr(settings, 'GUESTBOOK_PAGE', True)
+    return {
+        'GUESTBOOK_PAGE': guestbook_enabled,
+        'AUTHENTICATION_ENABLED': guestbook_enabled,  # Auth is only enabled when guestbook is enabled
+    }

6b6b6bcde04cce68d5c7015c9c25b13ea0a9d65d
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-07-02 19:33:44 +0700

    fix(project): set created_at date for project-44

--- apps/data/content/projects/project-44.py
+++ apps/data/content/projects/project-44.py
@@ -37,7 +37,7 @@ project_data = {
     "github_url": "https://github.com/ridwaanhall/ridwaanhall-com",
     "demo_url": "https://ridwaanhall.com",
     "status": "completed",
-    "created_at": None,
+    "created_at": datetime.strptime("2025-03-16T12:03:09+07:00", "%Y-%m-%dT%H:%M:%S%z"),
     "updated_at": datetime.strptime("2025-07-02T19:32:09+07:00", "%Y-%m-%dT%H:%M:%S%z"),
     "category": "portfolio",
     "tags": ["portfolio", "personal site", "django", "tailwindcss", "vercel", "dashboard"],

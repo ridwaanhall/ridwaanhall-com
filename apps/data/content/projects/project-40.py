e55d62d41b89c48fa713e9f03f098c2309f040b6
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-07-06 16:59:08 +0700

    update(project): adjust project metadata

--- apps/data/content/projects/project-40.py
+++ apps/data/content/projects/project-40.py
@@ -15,7 +15,7 @@ project_data = {
     "description": ['This dashboard dives into how weather drives bike rentals, breaking down trends by season, day, and conditions.', 'It uses data analysis to help bike companies optimize fleets and pricing based on forecasts.', 'Filter by temp, humidity, or time to uncover patterns, with ML models predicting future demand.'],
     "image_url": f"{settings.PROJECT_BASE_IMG_URL}/bike_sharing_analysis_dashboard.webp",
     "img_name": "bike_sharing_analysis_dashboard.webp",
-    "is_featured": True,
+    "is_featured": False,
     "features": [{'title': 'Season Trends', 'description': 'Shows peak biking seasons.'}, {'title': 'Weather Impact', 'description': 'Links conditions to rental spikes.'}, {'title': 'Day Breakdown', 'description': 'Compares weekdays vs. weekends.'}, {'title': 'Streamlit UI', 'description': 'Interactive charts for easy insights.'}],
     "tech_stack": [
         SkillsData.tech_stack["python"],
@@ -26,7 +26,7 @@ project_data = {
     "demo_url": "https://ridwaanhall-bike-sharing-analytics.streamlit.app/",
     "status": "completed",
     "created_at": None,
-    "updated_at": datetime.strptime("2025-04-17T17:49:00+07:00", "%Y-%m-%dT%H:%M:%S%z"),
+    "updated_at": datetime.strptime("2025-07-06T16:51:00+07:00", "%Y-%m-%dT%H:%M:%S%z"),
     "category": "Dashboard, Data Analysis, Bike Rental, Streamlit",
     "tags": [
         "Dashboard",

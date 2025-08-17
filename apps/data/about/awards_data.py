b4da0f4a2ad2487ad8d74855bcee4a8b11d440f1
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-08-17 20:10:41 +0700

    fix(awards): restructure issued dates to separate month and year fields

--- apps/data/about/awards_data.py
+++ apps/data/about/awards_data.py
@@ -8,7 +8,10 @@ class AwardsData:
             "title": "Certificate of Appreciation for Efforts in Memorizing the Quran",
             "credential_url": "https://drive.google.com/file/d/1-XJlFZzkq00QM9n2m7lh4m641nlT8jod/view",
             "description": "Certificate of Appreciation for Efforts in Memorizing the Quran",
-            "issued": "Feb 2020",
+            "issued": {
+                "month": "Feb",
+                "year": 2020
+            },
             "institution": "Islamic Boarding School of Al-Mukmin",
             "website": "https://almukminngruki.or.id/",
             "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
@@ -18,7 +21,10 @@ class AwardsData:
             "title": "Certificate of Scholarship for Memorizing 10 Juz of the Quran",
             "credential_url": "https://drive.google.com/file/d/1-L8O8h1ErOH2HqKFnMDDVazr-tEpJGEd/view",
             "description": "I was awarded the Tahfidz Scholarship Certificate for memorizing 10 Juz of the Quran after successfully passing the memorization test. This scholarship is a recognition of my dedication and effort in studying the Quran.",
-            "issued": "Mar 2020",
+            "issued": {
+                "month": "Mar",
+                "year": 2020
+            },
             "institution": "Islamic Boarding School of Al-Mukmin",
             "website": "https://almukminngruki.or.id/",
             "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
@@ -28,7 +34,10 @@ class AwardsData:
             "title": "Certificate of Appreciation for Leadership in Imarotu Syu'unith Tholabah (IST)",
             "credential_url": "https://drive.google.com/file/d/1-4ciNM2bKyR9fDWoS8t7gU30vkrqvtVc/view",
             "description": "Awarded for leadership and organizational contributions in IST, a student council-like organization at Al-Mukmin Islamic Boarding School.",
-            "issued": "Oct 2020",
+            "issued": {
+                "month": "Oct",
+                "year": 2020
+            },
             "institution": "Islamic Boarding School of Al-Mukmin",
             "website": "https://almukminngruki.or.id/",
             "logo": f"{settings.BASE_URL}/static/img/logo/al_mukmin_ngruki.webp",
@@ -38,7 +47,10 @@ class AwardsData:
             "title": "Community Top Voice in Programming",
             "credential_url": "https://www.linkedin.com/in/ridwaanhall/details/honors/",
             "description": "Recognized as a top 5% contributor in Programming on LinkedIn, showcasing expertise and insightful contributions in the field.",
-            "issued": "Jul 2024",
+            "issued": {
+                "month": "Jul",
+                "year": 2024
+            },
             "institution": "LinkedIn",
             "website": "https://www.linkedin.com/",
             "logo": f"{settings.BASE_URL}/static/img/logo/linkedin.webp",
@@ -48,7 +60,10 @@ class AwardsData:
             "title": "Community Top Voice in Machine Learning",
             "credential_url": "https://www.linkedin.com/in/ridwaanhall/details/honors/",
             "description": "Recognized as a top 5% contributor in Machine Learning on LinkedIn, showcasing expertise and insightful contributions in the field.",
-            "issued": "Aug 2024",
+            "issued": {
+                "month": "Aug",
+                "year": 2024
+            },
             "institution": "LinkedIn",
             "website": "https://www.linkedin.com/",
             "logo": f"{settings.BASE_URL}/static/img/logo/linkedin.webp",
@@ -58,7 +73,10 @@ class AwardsData:
             "title": "Community Top Voice in Informatics",
             "credential_url": "https://www.linkedin.com/in/ridwaanhall/details/honors/",
             "description": "Recognized as a top 5% contributor in Informatics on LinkedIn, showcasing expertise and insightful contributions in the field.",
-            "issued": "Oct 2024",
+            "issued": {
+                "month": "Oct",
+                "year": 2024
+            },
             "institution": "LinkedIn",
             "website": "https://www.linkedin.com/",
             "logo": f"{settings.BASE_URL}/static/img/logo/linkedin.webp",

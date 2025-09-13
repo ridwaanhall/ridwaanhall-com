4f8ac96b4cae3de849c1f33b0059e912f85aed08
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-13 18:06:54 +0700

    feat: update project-49 to set is_featured to False

--- apps/data/content/projects/project-49.py
+++ apps/data/content/projects/project-49.py
@@ -23,7 +23,7 @@ project_data = {
         "el_perintis_preview.webp": f"{settings.PROJECT_BASE_IMG_URL}/el_perintis_preview.webp",
         # "el_perintis.webp": f"{settings.PROJECT_BASE_IMG_URL}/el_perintis.webp"
     },
-    "is_featured": True,
+    "is_featured": False,
     "features": [
         {'title': 'OOP vs non-OOP Simulation', 'description': 'The `perintis` branch uses OOP to build a new foundation, while `pewaris` continues the existing system procedurally.'},
         {'title': 'Struggle Narrative', 'description': 'Python functions illustrate the journey, failures, and victories in a social and personal context.'},

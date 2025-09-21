d889d79af665992376c2d8a5ec664f0aa823d967
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-21 16:49:21 +0700

    feat(templates): add base_settings context processor to TEMPLATES

--- FlexForge/settings.py
+++ FlexForge/settings.py
@@ -265,6 +265,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'FlexForge.context_processors.feature_flags',
+                'FlexForge.context_processors.base_settings',
             ],
         },
     },
